16c039d02c7772a7d026df373f05a1283b19409c
bn0ir <gblacknoir@gmail.com>
2019-04-10 19:28:21 +0500

    Add redis cache basic implementation

--- cache.go
+++ cache.go
@@ -186,26 +186,67 @@ func (m *MemcachedCache) Full() bool {
 }
 
 /*
-TODO: Redis cache Backend
+Redis cache Backend
 */
 
+func NewRedisCache(rs RedisSettings, expire int64) *RedisCache {
+	rc := &redis.Client{Addr: rs.Addr(), Db: rs.DB, Password: rs.Password}
+	return &RedisCache{
+		Backend: rc,
+		Expire:  expire,
+	}
+}
+
 type RedisCache struct {
-	Backend    *redis.Client
-	Serializer JsonSerializer
-	Expire     time.Duration
-	Maxcount   int
+	Backend *redis.Client
+	Expire  int64
 }
 
-func (c *RedisCache) Get() {
-
+func (r *RedisCache) Get(key string) (*dns.Msg, error) {
+	var msg dns.Msg
+	item, err := r.Backend.Get(key)
+	if err != nil {
+		err = KeyNotFound{key}
+		return &msg, err
+	}
+	err = msg.Unpack(item)
+	if err != nil {
+		err = SerializerError{err}
+	}
+	return &msg, err
 }
 
-func (c *RedisCache) Set() {
+func (r *RedisCache) Set(key string, msg *dns.Msg) error {
+	var val []byte
+	var err error
 
+	// handle cases for negacache where it sets nil values
+	if msg == nil {
+		val = []byte("nil")
+	} else {
+		val, err = msg.Pack()
+	}
+	if err != nil {
+		err = SerializerError{err}
+	}
+	return r.Backend.Setex(key, r.Expire, val)
 }
 
-func (c *RedisCache) Remove() {
+func (r *RedisCache) Exists(key string) bool {
+	_, err := r.Backend.Get(key)
+	if err != nil {
+		return true
+	}
+	return false
+}
 
+func (r *RedisCache) Remove(key string) error {
+	_, err := r.Backend.Del(key)
+	return err
+}
+
+func (r *RedisCache) Full() bool {
+	return false
 }
 
 func KeyGen(q Question) string {
